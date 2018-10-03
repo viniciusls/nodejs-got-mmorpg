7e78b1f1f31d2e44389a007a9df3001dc0a8f306
Vinicius Silva <vinicius768@msn.com>
2018-10-03 00:03:09 -0300

    Implemented registration view/validation

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -1,3 +1,22 @@
 module.exports.create = (application, req, res) => {
-    res.render('cadastro');
+    res.render('cadastro', { errors: {}, data: {} });
 };
+
+module.exports.store = (application, req, res) => {
+    const data = req.body;
+
+    req.assert('name', 'Nome não pode ser vazio.').notEmpty();
+    req.assert('username', 'Usuário não pode ser vazio.').notEmpty();
+    req.assert('password', 'Senha não pode ser vazia.').notEmpty();
+    req.assert('house', 'Casa não pode ser vazia.').notEmpty();
+
+    const errors = req.validationErrors();
+
+    if (errors) {
+        res.render('cadastro', { errors, data });
+        return;
+    }
+
+    res.redirect('');
+};
+
--- app/routes/register.js
+++ app/routes/register.js
@@ -2,4 +2,8 @@ module.exports = (application) => {
     application.get('/register', (req, res) => {
         application.app.controllers.register.create(application, req, res);
     });
+
+    application.post('/register', (req, res) => {
+        application.app.controllers.register.store(application, req, res);
+    });
 };
