ba28540b2654d4e439e1f9eac6e33f561fc59050
Vinicius Silva <vinicius768@msn.com>
2018-10-03 12:22:34 -0300

    Added express session as a middleware

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+const expressSession = require('express-session');
 
 const app = express();
 
@@ -11,6 +12,11 @@ app.set('views', './app/views');
 app.use(express.static('./app/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
+app.use(expressSession({ 
+    secret: 'huasduasuhafauhduhda', 
+    resave: false, 
+    saveUninitialized: false 
+}));
 
 consign()
     .include('app/routes')
